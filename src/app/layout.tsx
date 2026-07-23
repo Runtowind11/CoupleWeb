1d2259481bade66f507054fcaf54aec3d5fe0d55
Runtowind11 <3275239616@qq.com>
2026-07-23 22:56:58 +0800

    观影平台添加浏览器标签页图标

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Our Story",
   description: "A love story website",
+  icons: {
+    icon: [{ url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎬</text></svg>", type: 'image/svg+xml' }],
+  },
 };
 
 export default function RootLayout({
